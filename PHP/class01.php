596485cab9cc15404d8421e18f3295f2b2d5e94c
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-07 10:46:56 +0500

    Create class01.php

---
+++ PHP/class01.php
@@ -0,0 +1,22 @@
+           <!-- Basic Syntax -->
+
+<?php
+
+    # php code goes Here
+?>
+
+     <!-- Comments in PHP -->
+
+<?php
+
+ // This is a single-line comment
+
+
+
+ # This is also a single-line comment
+
+
+ /* This is a
+multi-line comment */
+
+?>
