4058003a0b00a5552d9a3ca0bb129fef9b1a9421
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-09 11:17:02 +0500

    Create class02.php

---
+++ PHP/class02.php
@@ -0,0 +1,23 @@
+<?php
+
+// echo "Hello World";
+
+    // Variables in PHP
+
+$name = "Amina";
+$age  = "25";
+$Height = "5.4";
+$isStdent = "true";
+
+echo $name;
+echo $age;
+echo $Height;
+echo $isStdent;
+
+$num1 = 2.567;
+$num2 = 2.678;
+$few = $num1 + $num2;
+
+echo $few;
+
+?>
