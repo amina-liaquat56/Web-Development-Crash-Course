1e0ca6900f294a0da66bd6856a4233bd7cb78ea1
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-11 10:29:33 +0500

    Update class04.php

--- PHP/class04.php
+++ PHP/class04.php
@@ -77,6 +77,40 @@
     for ($i = 1; $i <= 5; $i++) {
     echo "Iteration: $i <br>";
     }
+#for loop
+$a = 0; 
+$b = 0; 
+for( $i=0; $i<5; $i++ ) 
+{  
+    #$a = $a+10
+$a += 10; 
+$b += 5; 
+} 
+echo ("At the end of the loop a=$a and b=$b" ); 
+
+#while loop
+$i = 1;
+
+while ($i < 6) {
+  echo $i;
+  echo"<br>";
+  $i++;
+} 
+
+#do while loop
+do {
+  echo $i;
+  echo"<br>";
+  $i++;
+} while ($i < 6);
+
+#foreach loop
+
+$colors = array("red", "green", "blue", "yellow"); 
+
+foreach ($colors as $x) {
+  echo "$x <br>";
+}
           
     ?>
 </body>
